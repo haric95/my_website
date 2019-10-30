cd28f45e01c743b2686c8cb61e8c8efdb0bbff37
haric95 <hari-c@hotmail.co.uk>
2019-10-30 02:31:35 +0000

    noise!

--- landing.js
+++ landing.js
@@ -1,4 +1,18 @@
 var inp = 0
+var rand_i;
+var rand_j;
+var num_x;
+var num_y;
+
+
+let attackLevel = 1.0;
+let releaseLevel = 0;
+let attackTime = 0.001;
+let decayTime = 0.2;
+let susPercent = 0.2;
+let releaseTime = 0.5;
+let env, triOsc;
+
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
@@ -6,6 +20,15 @@ function setup() {
     ellipseMode(RADIUS);
     frameRate(60);
     blendMode(HARD_LIGHT);
+    env = new p5.Envelope();
+    env.setADSR(attackTime, decayTime, susPercent, releaseTime);
+    env.setRange(attackLevel, releaseLevel);
+    triOsc = new p5.Oscillator('triangle');
+    triOsc.amp(env);
+    triOsc.start();
+    triOsc;
+
+    
 }
 
 function windowResized() {
@@ -15,7 +38,11 @@ function windowResized() {
 }
 
 function keyPressed() {
-    fill(0,0,150);
+    triOsc.freq(220 + rand_i*10 + rand_j*30);
+    rand_i = Math.floor(Math.random() * (num_x + 1));
+    rand_j = Math.floor(Math.random() * (num_y + 1));
+    env.play();
+
 }
 
 function draw() {
@@ -32,13 +59,18 @@ function draw() {
     for (i = 0; i < num_x; i++) {
         for (j = 0; j < num_y; j++) {
             fill(170 - i*10,120 - j*3,180);
+            
             x_coord = (x_offset) + radius + i*radius*2
             y_coord =  (y_offset) + radius + j*radius*2
             var distance = dist(mouseX, mouseY, x_coord, y_coord);
+            if (i == rand_i && j == rand_j) {
+                fill(100,100,100);
+                distance = 10;
+            }
             var size_change = 20*(sin(-inp + distance/100));
             ellipse(x_coord, y_coord, radius-distance/15 + size_change, radius-distance/15 + size_change);
             noFill();
-            stroke(10);
+            stroke(30);
             ellipse(x_coord+5, y_coord-8, radius-distance/15 + size_change, radius-distance/15 + size_change);
 
         }
